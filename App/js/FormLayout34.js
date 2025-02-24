525edf5fbc9e983236b9b1a15309f40a41ad9c52
zacharymitrevski <mitrevskizachary@gmail.com>
2025-02-24 16:34:00 +1100

    Updated by CrossUI No-Code App Builder

--- App/js/FormLayout34.js
+++ App/js/FormLayout34.js
@@ -9,193 +9,193 @@ xui.Class('App.FormLayout34', 'xui.Module',{
                 .setHost(host,"xui_ui_formlayout1")
                 .setLeft("5.833333333333333em")
                 .setTop("1.6666666666666667em")
-                .setWidth("56.666666666666664em")
-                .setHeight("33.333333333333336em")
+                .setWidth("56.68571428571428em")
+                .setHeight("33.2952380952381em")
                 .setLayoutData({
-                    "rows":5,
-                    "cols":6,
-                    "merged":[
+                    "rows" : 5,
+                    "cols" : 6,
+                    "merged" : [
                         {
-                            "row":1,
-                            "col":1,
-                            "rowspan":1,
-                            "colspan":2,
-                            "removed":false
+                            "row" : 1,
+                            "col" : 1,
+                            "rowspan" : 1,
+                            "colspan" : 2,
+                            "removed" : false
                         },
                         {
-                            "row":2,
-                            "col":1,
-                            "rowspan":1,
-                            "colspan":2,
-                            "removed":false
+                            "row" : 2,
+                            "col" : 1,
+                            "rowspan" : 1,
+                            "colspan" : 2,
+                            "removed" : false
                         },
                         {
-                            "row":1,
-                            "col":4,
-                            "rowspan":1,
-                            "colspan":2,
-                            "removed":false
+                            "row" : 1,
+                            "col" : 4,
+                            "rowspan" : 1,
+                            "colspan" : 2,
+                            "removed" : false
                         },
                         {
-                            "row":2,
-                            "col":4,
-                            "rowspan":1,
-                            "colspan":2,
-                            "removed":false
+                            "row" : 2,
+                            "col" : 4,
+                            "rowspan" : 1,
+                            "colspan" : 2,
+                            "removed" : false
                         },
                         {
-                            "row":3,
-                            "col":1,
-                            "rowspan":1,
-                            "colspan":5,
-                            "removed":false
+                            "row" : 3,
+                            "col" : 1,
+                            "rowspan" : 1,
+                            "colspan" : 5,
+                            "removed" : false
                         },
                         {
-                            "row":4,
-                            "col":1,
-                            "rowspan":1,
-                            "colspan":5,
-                            "removed":false
+                            "row" : 4,
+                            "col" : 1,
+                            "rowspan" : 1,
+                            "colspan" : 5,
+                            "removed" : false
                         },
                         {
-                            "row":0,
-                            "col":0,
-                            "rowspan":1,
-                            "colspan":6,
-                            "removed":false
+                            "row" : 0,
+                            "col" : 0,
+                            "rowspan" : 1,
+                            "colspan" : 6,
+                            "removed" : false
                         }
                     ],
-                    "rowSetting":{
-                        "1":{
-                            "manualHeight":50
+                    "rowSetting" : {
+                        "1" : {
+                            "manualHeight" : 50
                         },
-                        "2":{
-                            "manualHeight":33
+                        "2" : {
+                            "manualHeight" : 33
                         },
-                        "3":{
-                            "manualHeight":32
+                        "3" : {
+                            "manualHeight" : 32
                         },
-                        "4":{
-                            "manualHeight":174
+                        "4" : {
+                            "manualHeight" : 174
                         },
-                        "5":{
-                            "manualHeight":24
+                        "5" : {
+                            "manualHeight" : 24
                         }
                     },
-                    "colSetting":{
-                        "E":{
-                            "manualWidth":50
+                    "colSetting" : {
+                        "E" : {
+                            "manualWidth" : 50
                         }
                     },
-                    "cells":{
-                        "A1":{
-                            "value":"Staff Form",
-                            "style":{
-                                "textAlign":"center",
-                                "fontSize":"20px",
-                                "fontFamily":"arial black,avant garde"
+                    "cells" : {
+                        "A1" : {
+                            "value" : "Student",
+                            "style" : {
+                                "textAlign" : "center",
+                                "fontSize" : "20px",
+                                "fontFamily" : "arial black,avant garde"
                             }
                         },
-                        "A2":{
-                            "value":"Name"
+                        "A2" : {
+                            "value" : "Name"
                         },
-                        "D2":{
-                            "value":"Birthday"
+                        "D2" : {
+                            "value" : "Birthday"
                         },
-                        "A3":{
-                            "value":"Gender"
+                        "A3" : {
+                            "value" : "Gender"
                         },
-                        "D3":{
-                            "value":"Style"
+                        "D3" : {
+                            "value" : "Style"
                         },
-                        "A4":{
-                            "value":"Desc"
+                        "A4" : {
+                            "value" : "Desc"
                         },
-                        "A5":{
-                            "value":"Memo",
-                            "style":{
-                                "backgroundColor":"#FFE4B5"
+                        "A5" : {
+                            "value" : "Memo",
+                            "style" : {
+                                "backgroundColor" : "#FFE4B5"
                             }
                         },
-                        "B5":{
-                            "value":"This is a form layout demo!",
-                            "style":{
-                                "textAlign":"left",
-                                "verticalAlign":"bottom",
-                                "fontStyle":"italic",
-                                "color":"#BC8F8F",
-                                "backgroundColor":"#FFE4B5"
+                        "B5" : {
+                            "value" : "This is a form layout demo!",
+                            "style" : {
+                                "textAlign" : "left",
+                                "verticalAlign" : "bottom",
+                                "fontStyle" : "italic",
+                                "color" : "#BC8F8F",
+                                "backgroundColor" : "#FFE4B5"
                             }
                         },
-                        "B1":{
-                            "style":{
-                                "textAlign":"center",
-                                "fontSize":"20px",
-                                "fontFamily":"arial black,avant garde"
+                        "B1" : {
+                            "style" : {
+                                "textAlign" : "center",
+                                "fontSize" : "20px",
+                                "fontFamily" : "arial black,avant garde"
                             }
                         },
-                        "C1":{
-                            "style":{
-                                "textAlign":"center",
-                                "fontSize":"20px",
-                                "fontFamily":"arial black,avant garde"
+                        "C1" : {
+                            "style" : {
+                                "textAlign" : "center",
+                                "fontSize" : "20px",
+                                "fontFamily" : "arial black,avant garde"
                             }
                         },
-                        "D1":{
-                            "style":{
-                                "textAlign":"center",
-                                "fontSize":"20px",
-                                "fontFamily":"arial black,avant garde"
+                        "D1" : {
+                            "style" : {
+                                "textAlign" : "center",
+                                "fontSize" : "20px",
+                                "fontFamily" : "arial black,avant garde"
                             }
                         },
-                        "E1":{
-                            "style":{
-                                "textAlign":"center",
-                                "fontSize":"20px",
-                                "fontFamily":"arial black,avant garde"
+                        "E1" : {
+                            "style" : {
+                                "textAlign" : "center",
+                                "fontSize" : "20px",
+                                "fontFamily" : "arial black,avant garde"
                             }
                         },
-                        "F1":{
-                            "style":{
-                                "textAlign":"center",
-                                "fontSize":"20px",
-                                "fontFamily":"arial black,avant garde"
+                        "F1" : {
+                            "style" : {
+                                "textAlign" : "center",
+                                "fontSize" : "20px",
+                                "fontFamily" : "arial black,avant garde"
                             }
                         },
-                        "C5":{
-                            "style":{
-                                "textAlign":"left",
-                                "verticalAlign":"bottom",
-                                "fontStyle":"italic",
-                                "color":"#BC8F8F",
-                                "backgroundColor":"#FFE4B5"
+                        "C5" : {
+                            "style" : {
+                                "textAlign" : "left",
+                                "verticalAlign" : "bottom",
+                                "fontStyle" : "italic",
+                                "color" : "#BC8F8F",
+                                "backgroundColor" : "#FFE4B5"
                             }
                         },
-                        "D5":{
-                            "style":{
-                                "textAlign":"left",
-                                "verticalAlign":"bottom",
-                                "fontStyle":"italic",
-                                "color":"#BC8F8F",
-                                "backgroundColor":"#FFE4B5"
+                        "D5" : {
+                            "style" : {
+                                "textAlign" : "left",
+                                "verticalAlign" : "bottom",
+                                "fontStyle" : "italic",
+                                "color" : "#BC8F8F",
+                                "backgroundColor" : "#FFE4B5"
                             }
                         },
-                        "E5":{
-                            "style":{
-                                "textAlign":"left",
-                                "verticalAlign":"bottom",
-                                "fontStyle":"italic",
-                                "color":"#BC8F8F",
-                                "backgroundColor":"#FFE4B5"
+                        "E5" : {
+                            "style" : {
+                                "textAlign" : "left",
+                                "verticalAlign" : "bottom",
+                                "fontStyle" : "italic",
+                                "color" : "#BC8F8F",
+                                "backgroundColor" : "#FFE4B5"
                             }
                         },
-                        "F5":{
-                            "style":{
-                                "textAlign":"left",
-                                "verticalAlign":"bottom",
-                                "fontStyle":"italic",
-                                "color":"#BC8F8F",
-                                "backgroundColor":"#FFE4B5"
+                        "F5" : {
+                            "style" : {
+                                "textAlign" : "left",
+                                "verticalAlign" : "bottom",
+                                "fontStyle" : "italic",
+                                "color" : "#BC8F8F",
+                                "backgroundColor" : "#FFE4B5"
                             }
                         }
                     }
@@ -208,8 +208,8 @@ xui.Class('App.FormLayout34', 'xui.Module',{
                 .setName("B2")
                 .setLeft("0em")
                 .setTop("0em")
-                .setWidth("20.416666666666668em")
-                .setHeight("2.6666666666666665em")
+                .setWidth("20.038095238095238em")
+                .setHeight("2.4380952380952383em")
                 .setLabelPos("none"),
                 "B2"
             );
@@ -220,11 +220,11 @@ xui.Class('App.FormLayout34', 'xui.Module',{
                 .setName("E2")
                 .setLeft("0em")
                 .setTop("0em")
-                .setWidth("14.333333333333334em")
-                .setHeight("2.6666666666666665em")
+                .setWidth("13.866666666666667em")
+                .setHeight("2.4380952380952383em")
                 .setLabelPos("none")
                 .setType("date")
-                .setValue("106372800000"),
+                .setValue("106322400000"),
                 "E2"
             );
             
@@ -234,18 +234,18 @@ xui.Class('App.FormLayout34', 'xui.Module',{
                 .setName("B3")
                 .setItems([
                     {
-                        "id":"m",
-                        "caption":"Male"
+                        "id" : "m",
+                        "caption" : "Male"
                     },
                     {
-                        "id":"f",
-                        "caption":"Femail"
+                        "id" : "f",
+                        "caption" : "Femail"
                     }
                 ])
                 .setLeft("0em")
                 .setTop("0em")
-                .setWidth("20.416666666666668em")
-                .setHeight("2.5833333333333335em")
+                .setWidth("20.038095238095238em")
+                .setHeight("2.361904761904762em")
                 .setLabelPos("none")
                 .setLabelHAlign("left")
                 .setValue("a"),
@@ -258,8 +258,8 @@ xui.Class('App.FormLayout34', 'xui.Module',{
                 .setName("E3")
                 .setLeft("0em")
                 .setTop("0em")
-                .setWidth("14.333333333333334em")
-                .setHeight("2.5833333333333335em")
+                .setWidth("13.866666666666667em")
+                .setHeight("2.361904761904762em")
                 .setLabelPos("none")
                 .setType("color"),
                 "E3"
@@ -271,8 +271,8 @@ xui.Class('App.FormLayout34', 'xui.Module',{
                 .setName("B4")
                 .setLeft("0em")
                 .setTop("0em")
-                .setWidth("46.083333333333336em")
-                .setHeight("14.416666666666666em")
+                .setWidth("45.714285714285715em")
+                .setHeight("13.18095238095238em")
                 .setLabelPos("none")
                 .setLabelHAlign("left"),
                 "B4"
