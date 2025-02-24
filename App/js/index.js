732dfe518dc997e6fecab7887978c3486184af03
zacharymitrevski <mitrevskizachary@gmail.com>
2025-02-24 16:58:40 +1100

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -24,7 +24,7 @@ xui.Class('App', 'xui.Module',{
                 xui.create("xui.UI.Label")
                 .setHost(host,"xui_ui_label7")
                 .setLeft("0em")
-                .setTop("0.35714285714285715em")
+                .setTop("1.0714285714285714em")
                 .setWidth("6.928571428571429em")
                 .setHeight("3.5047619047619047em")
                 .setCaption("Gateway 2025")
@@ -40,7 +40,7 @@ xui.Class('App', 'xui.Module',{
                 xui.create("xui.UI.Button")
                 .setHost(host,"xui_ui_button6")
                 .setLeft("0.7619047619047619em")
-                .setTop("4.571428571428571em")
+                .setTop("9.904761904761905em")
                 .setWidth("12.876190476190477em")
                 .setCaption("Add Student")
                 .setFontColor("#000000")
@@ -55,6 +55,101 @@ xui.Class('App', 'xui.Module',{
                         "event" : 1
                     }
                 ])
+            );
+            
+            append(
+                xui.create("xui.UI.MenuBar")
+                .setHost(host,"xui_ui_menubar2")
+                .setItems([
+                    {
+                        "id" : "menu1",
+                        "sub" : [
+                            {
+                                "id" : "normal",
+                                "caption" : "normal"
+                            },
+                            {
+                                "id" : "disabled",
+                                "caption" : "disabled",
+                                "disabled" : true
+                            },
+                            {
+                                "id" : "image",
+                                "caption" : "image",
+                                "imageClass" : "xui-icon-xui"
+                            },
+                            {
+                                "type" : "split"
+                            },
+                            {
+                                "id" : "checkbox 1",
+                                "caption" : "checkbox 1",
+                                "type" : "checkbox"
+                            },
+                            {
+                                "id" : "checkbox 2",
+                                "caption" : "checkbox 2",
+                                "type" : "checkbox"
+                            }
+                        ],
+                        "caption" : "menu1"
+                    },
+                    {
+                        "id" : "menu2",
+                        "sub" : [
+                            {
+                                "id" : "sub menu 1",
+                                "caption" : "sub menu 1",
+                                "add" : "[Ctrl+F]",
+                                "sub" : [
+                                    {
+                                        "id" : "sub 1",
+                                        "type" : "radiobox"
+                                    },
+                                    {
+                                        "id" : "sub 2",
+                                        "type" : "radiobox"
+                                    },
+                                    {
+                                        "id" : "sub 3"
+                                    }
+                                ]
+                            },
+                            {
+                                "id" : "sub menu 2",
+                                "caption" : "sub menu 2",
+                                "add" : "[Ctrl+T]",
+                                "sub" : [
+                                    "sub 3",
+                                    "sub 4"
+                                ]
+                            }
+                        ],
+                        "caption" : "menu2"
+                    }
+                ])
+                .setTop("33.523809523809526em")
+                .onMenuSelected([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "page",
+                        "target" : "App.add student",
+                        "args" : [
+                            "{page.popUp()}"
+                        ],
+                        "method" : "popUp",
+                        "redirection" : "page::"
+                    }
+                ])
+            );
+            
+            append(
+                xui.create("xui.UI.PageBar")
+                .setHost(host,"xui_ui_pagebar1")
+                .setLeft("0em")
+                .setTop("43.42857142857143em")
+                .setWidth("14.628571428571428em")
+                .setCaption("PageBar")
             );
             
             return children;
